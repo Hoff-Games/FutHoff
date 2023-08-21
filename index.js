7f45727c961f73bac11b72464389ff2ce5c23326
PamIsa Games <pamella.lh@aluno.ifsc.edu.br>
2023-08-21 20:23:45 +0000

    Configuração do jogo com Phaser #17

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 /*Importação de objetos*/
 import config from './config.js'
+import cena0 from './cena0.js'
 
 /*Definição do objeti Game*/
 class Game extends Phaser.Game {
     constructor () {
         super(config)
+
+        this.scene.add('cena0', cena0)
+
+        this.scene.start('cena0')
     }
 }
 /*Criação do objeto Game*/
